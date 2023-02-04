af4d1821a7e4a10bf953f69afd8708f23b53ecf5
Noa/Nocly/Enzo <enzo.bedos.13@gmail.com>
2023-02-04 15:30:30 +0100

    Creation des fenetres modales (ouverture/fermeture)

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.window__modal'),
+	  modalBtns = document.querySelectorAll('.link__title'),
+	  modalCloses = document.querySelectorAll('.window__xp-files-button--close')
+
+let modal = function(modalClick){
+	modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) =>{
+	modalBtn.addEventListener('click', () =>{
+		modal(i)
+	})
+})
+
+modalCloses.forEach((modalClose) =>{
+	modalClose.addEventListener('click', () =>{
+		modalViews.forEach((modalView) =>{
+			modalView.classList.remove('active-modal')
+		})
+	})
+})
