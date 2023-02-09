7f2fb365a780019448ffc6b60c8f81ee695164a2
Enzo BEDOS <enzo.bedos@etu.univ-amu.fr>
2023-02-09 12:14:27 +0100

    finir mdp xp-files

--- JS/script.js
+++ JS/script.js
@@ -43,3 +43,26 @@ function LoLAlert() {
 	alert("Tu crois que c'est le moment de jouer ?! \nApres avoir trouve le virus t'ira prendre une douche \nparce que la sa pue !");
 }
 
+// MDP
+
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+const modalViewPwd = document.querySelectorAll('.window__mdp');
+const modalXp = document.querySelectorAll('.window__xp-files');
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    const password = loginForm.password.value;
+
+    if (password === "test") {
+        // alert("You have successfully logged in.");
+        modalViewPwd.classList.remove('active-modal')
+		modalXp.classList.add('active-modal')
+
+    } else {
+        alert('Mot de passe incorrect ...')
+    }
+})
