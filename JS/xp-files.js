a1ca8f0e6c028fbe35914568ad986f56e4f01815
Enzo BEDOS <enzo.bedos@etu.univ-amu.fr>
2023-02-15 17:19:36 +0100

    Quasiment terminer (form)

--- JS/xp-files.js
+++ JS/xp-files.js
@@ -66,4 +66,34 @@ modalClosesMenu.forEach((modalCloseMenu) =>{
 	})
 })
 
-const Torrent = document.querySelectorAll('item__menu-option--torrent')
+
+var nClicks    = 0;
+var nMaxClicks = 3;
+
+function Clicking() {
+    nClicks++;
+
+    if (nClicks >= nMaxClicks){
+       
+       alert("On dirais que le fichier ne veux pas s'ouvrir \nEssaye de trouver une autre solution.");   
+    }
+}
+
+const GTA6Deleted = document.getElementById("GTA6");
+const GTA6Trash = document.getElementById('GTA6Trash')
+const textTC = document.getElementById('text__trashcan');
+const BtnDelete = document.getElementById('section__delete')
+document.getElementById("delete").onclick = function() {
+	GTA6Deleted.style.visibility = "none";
+	GTA6Deleted.style.display = "none";
+
+	GTA6Trash.style.visibility = "visible";
+	GTA6Trash.style.display = "flex";
+
+	textTC.style.display ="none";
+	textTC.style.visibility="none";
+
+	BtnDelete.style.visibility = "visible";
+	BtnDelete.style.display = "flex";
+
+}
